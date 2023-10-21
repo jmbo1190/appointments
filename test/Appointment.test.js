045ef8e0d9b4f05db30a3c6b4bb60cbe9a9750de
Jean-Michel Bodart <jmbodart@skynet.be>
2023-10-21 13:00:45 +0200

    Make second test pass
    Use prop in Appointment component rather than hardcoded string
    Use document.ReplaceChildren() in test
    rather than document.appendChild()
    to make the tests independents.
    Note the second test had to be temporarily skipped using it.skip()
    to make the test suite pass again
    before updating the first test.

--- test/Appointment.test.js
+++ test/Appointment.test.js
@@ -10,15 +10,30 @@ describe("Appointment", () => {
         const customer = { firstName: "Ashley" };
         const component = <Appointment customer={ customer } />;
         const container = document.createElement("div");
-        document.body.appendChild(container);
+        // document.body.appendChild(container);
+        document.body.replaceChildren(container);
 
         // using act to wait for asynchronous render() to complete before testing
         act( () => {
             ReactDOM.createRoot(container).render(component);
         });
 
-        
         expect(document.body.textContent).toContain("Ashley");
+    })
+
+    it("renders another customer's first name", () => {
+        const customer = { firstName: "Jordan" };
+        const component = <Appointment customer={ customer } />;
+        const container = document.createElement("div");
+        // document.body.appendChild(container);
+        document.body.replaceChildren(container);
+
+        // using act to wait for asynchronous render() to complete before testing
+        act( () => {
+            ReactDOM.createRoot(container).render(component);
+        });
+
+        expect(document.body.textContent).toContain("Jordan");
     })
 
 });
