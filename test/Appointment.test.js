8a53d19f9b9b96054aeea4916b86921eaa015def
Jean-Michel Bodart <jmbodart@skynet.be>
2023-10-21 16:54:17 +0200

    AppointmentsDayView renders a div with the right ID

--- test/Appointment.test.js
+++ test/Appointment.test.js
@@ -2,10 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { act } from "react-dom/test-utils";
 
-import { Appointment } from "../src/Appointment";
+import { Appointment, AppointmentsDayView } from "../src/Appointment";
+
+
 
 describe("Appointment", () => {
-
 
     let container;
 
@@ -22,19 +23,38 @@ describe("Appointment", () => {
 
     it("renders the customer's first name", () => {
         const customer = { firstName: "Ashley" };
-        
         render( <Appointment customer={ customer } /> );
-
         expect(document.body.textContent).toContain("Ashley");
     })
 
     it("renders another customer's first name", () => {
         const customer = { firstName: "Jordan" };
-        
         render( <Appointment customer={ customer } /> );
-
         expect(document.body.textContent).toContain("Jordan");
     })
+
+});
+
+
+describe("AppointmentsDayView", () => {
+
+    let container;
+
+    beforeEach(() => {
+        container = document.createElement("div");
+        // document.body.appendChild(container);
+        document.body.replaceChildren(container);
+    });
+
+    const render = (component) => {
+        // using act to wait for asynchronous render() to complete before testing
+        act( () => ReactDOM.createRoot(container).render(component) )
+    };
+
+    it("renders a div with the right ID", () => {
+        render(<AppointmentsDayView appointments={ [] }/>);
+        expect(document.querySelector("div#appointmentsDayView")).not.toBeNull();
+    });
 
 });
 
