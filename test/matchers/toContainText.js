a88641bc9d0642844f6b5a0d21f08bef22ac9306
Jean-Michel Bodart <jmbodart@skynet.be>
2023-10-22 12:54:54 +0200

    Add matcher toContainText()

--- test/matchers/toContainText.js
+++ test/matchers/toContainText.js
@@ -1,7 +1,34 @@
+import {
+    matcherHint,
+    printExpected,
+    printReceived,
+  } from "jest-matcher-utils";
+
 export const toContainText = (
     received,
     expectedText
-  ) => ({
-    pass: true
-  });
+  ) => {
+    const pass = received.textContent.includes(expectedText);
+    const sourceHint = () =>
+    matcherHint(
+      "toContainText",
+      "element",
+      printExpected(expectedText),
+      { isNot: pass }
+    );
+    const actualTextHint = () =>
+        "Actual text: " + printReceived(received.textContent);
+
+    // const message = () => `expect(element).toContainText("text to find")`;
+    // const message = () =>
+    //     matcherHint(
+    //     "toContainText",
+    //     "element",
+    //     printExpected(expectedText),
+    //     { isNot: pass }
+    //     );
+    const message = () =>
+        [sourceHint(), actualTextHint()].join("\n\n");
+    return { pass, message };
+  };
   
