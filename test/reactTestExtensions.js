0c125f77543b4577c9ffa6de1f678a923d65f35c
Jean-Michel Bodart <jmbodart@skynet.be>
2023-10-22 21:31:29 +0200

    Update reactTestExtensions.js
    Add helper function change()

--- test/reactTestExtensions.js
+++ test/reactTestExtensions.js
@@ -40,8 +40,16 @@ export const field = (fieldName) => form().elements[fieldName];
 export const submitButton = () => element("input[type=submit]");
 
 
-export const originalValueProperty = (reactElement) => {
+const originalValueProperty = (reactElement) => {
     const prototype = Object.getPrototypeOf(reactElement);
     return Object.getOwnPropertyDescriptor( prototype, "value" );
-  };
+};
+
+
+export const change = (target, value) => {
+    originalValueProperty(target).set.call( target, value );
+    const event = new Event("change", { target, bubbles: true, });
+    act(() => target.dispatchEvent(event));
+};
+  
   
