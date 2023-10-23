e65ee9366172a1ec7eac8e539d478fe533f66e59
Jean-Michel Bodart <jmbodart@skynet.be>
2023-10-23 11:04:47 +0200

    Update index.js to show the CustomerForm component

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { AppointmentsDayView } from "./AppointmentsDayView";
+import { CustomerForm } from "./CustomerForm";
 import { sampleAppointments } from "./sampleData";
 
 ReactDOM.createRoot(
   document.getElementById("root")
 ).render(
-  <AppointmentsDayView appointments={sampleAppointments} />
+  // <AppointmentsDayView appointments={sampleAppointments} />
+  <CustomerForm original={ { firstName: "", lastName: "", phoneNumber: "" } } onSubmit={ (arg) => console.log(arg)} />
 );
